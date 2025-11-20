5a0cf74f26bfa1821a3c3eb9ea2286a998d3a742
yvesarthurdougueli-spec <yvesarthurdougueli@gmail.com>
2025-11-20 03:10:39 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Effet smooth scroll
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
